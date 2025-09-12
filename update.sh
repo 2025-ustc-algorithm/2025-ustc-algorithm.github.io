8433a8f299697eb1e428f43d153cc67888e1f3e1
VansWaston <825164707@qq.com>
2025-09-12 14:04:19 +0800

    update

--- update.sh
+++ update.sh
@@ -1,9 +1,13 @@
+# main分支为web分支，source分支为同步分支
 message="update"
 
 # git update
 git add -A
 git commit -m $message
-git push origin main --force
+git push origin main:main main:source --force
 
 # hexo update
 npx hexo clean && npx hexo generate && npx hexo deploy
+
+# git sync
+# git pull origin source
