ab7026d582ac374fa91c06bb812939354bf0f1e4
VansWaston <825164707@qq.com>
2025-10-30 18:52:15 +0800

    update

--- update.sh
+++ update.sh
@@ -1,17 +1,14 @@
 # main分支为web分支，source分支为同步分支
-if [ $# -gt 0 ]; then
-    message=$1
-else
-    message="update web"
-fi
+message="update web"
 
 # git update
 git add -A
 git commit -m $message
-git push origin main:main main:source --force
+git push origin source:main --force
+git push origin source:source --force
 
 # hexo update
-npx hexo clean && npx hexo generate && npx hexo deploy
+# npx hexo clean && npx hexo generate && npx hexo deploy
 
 # git sync
 # git pull origin source
